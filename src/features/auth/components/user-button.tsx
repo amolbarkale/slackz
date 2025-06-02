6b3b9612b2fa31bb5efab2fdb671ddf21cc8b2c6
amolbarkale <barkaleamol@gmail.com>
2025-06-03 02:36:55 +0530

    feat: frontend and backend setup with react and convex DB.

---
+++ src/features/auth/components/user-button.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useCurrentUser } from "../api/useCurrentUser";
+import { Loader, LogOutIcon } from "lucide-react";
+import { useAuthActions } from "@convex-dev/auth/react";
+
+export const UserButton = () => {
+  const { signOut } = useAuthActions();
+  const { data: user, isLoading } = useCurrentUser();
+
+  if (isLoading) {
+    return <Loader className="size-4 animate-spin text-muted-foreground" />;
+  }
+
+  if (!user) {
+    return null;
+  }
+
+  return (
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger className="outline-none relative">
+        <Avatar className="rounded-md size-10 hover:opacity-75 transition">
+          <AvatarImage
+            className="rounded-md"
+            alt={user.name}
+            src={user.image}
+          />
+          <AvatarFallback className="rounded-md bg-sky-500 text-white ">
+            {user.name!.charAt(0).toUpperCase()}
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="center" side="right" className="w-60">
+        <DropdownMenuItem onClick={() => signOut()} className="h-10">
+          <LogOutIcon className="size-4 mr-2" />
+          Log out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
